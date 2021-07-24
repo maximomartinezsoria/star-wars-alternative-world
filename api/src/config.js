4b55c53aaae341be05b0c86a9f36bfe495861e2b
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-24 13:43:07 -0300

    [Api] make env variables available

---
+++ api/src/config.js
@@ -0,0 +1,8 @@
+export const db = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+}
+
+export const port = process.env.API_PORT
--- api/src/startApolloServer.js
+++ api/src/startApolloServer.js
@@ -1,5 +1,6 @@
 import Koa from 'koa'
 import * as ApolloServerPkg from 'apollo-server-koa'
+import { port } from './config.js'
 
 const { ApolloServer } = ApolloServerPkg
 
@@ -11,7 +12,7 @@ export default async function startApolloServer(typeDefs, resolvers) {
   await server.start()
   const app = new Koa()
   server.applyMiddleware({ app })
-  await new Promise((resolve) => app.listen(3000, resolve))
-
+  await new Promise((resolve) => app.listen(port, resolve))
+  console.log(`Server listening on port ${port}`)
   return { server, app }
 }
