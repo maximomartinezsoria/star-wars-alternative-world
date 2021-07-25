13a8f5538b097564e5d23708f7d5aeff8773a821
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 01:53:05 -0300

    [Api] define Knex config

--- api/src/config.js
+++ api/src/config.js
@@ -3,12 +3,18 @@ import dotenv from 'dotenv'
 
 dotenv.config({ path: path.resolve('../.env') })
 
-export const db = {
+const db = {
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 5432,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+}
+
+export const knexConfig = {
+  client: 'pg',
+  connection: db,
+  pool: { min: 0, max: 10 },
 }
 
 export const port = process.env.API_PORT
