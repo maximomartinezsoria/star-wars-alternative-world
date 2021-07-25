d5e3cafcec59556d750d48b856b61bd07c47b56e
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 16:30:13 -0300

    [App] button styles

--- app/src/styles/GlobalStyles.js
+++ app/src/styles/GlobalStyles.js
@@ -6,10 +6,26 @@ const GlobalStyles = createGlobalStyle`
     --white: #fff;
     --gray: #f5f5f5;
     --dark-gray: rgba(18, 28, 51, 0.05);
+    --border-radius: 8px;
   }
 
   body {
     background-color: var(--gray);
+  }
+
+  button {
+    padding: 1.2rem 2.4rem;
+    background-color: var(--primary);
+    color: var(--white);
+    font-weight: 600;
+    text-transform: uppercase;
+    border: 0;
+    border-radius: var(--border-radius);
+
+    &.light {
+      background-color: var(--dark-gray);
+      color: var(--primary);
+    }
   }
 `
 
