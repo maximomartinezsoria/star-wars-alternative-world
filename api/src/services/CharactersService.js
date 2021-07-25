cadff855cb526ee89a68c0b45f119785f1f9c89e
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 01:56:40 -0300

    [Api] Create CharactersService

---
+++ api/src/services/CharactersService.js
@@ -0,0 +1,65 @@
+import { SQLDataSource } from 'datasource-sql'
+import { createCharacterWithPlanetFromDbResponse } from '../util/createFromDbResponse.js'
+
+class CharactersService extends SQLDataSource {
+  async getAllCharacters(
+    page = 1,
+    pageSize = 10,
+    planet = null,
+    birthDate = null
+  ) {
+    const charactersData = await this.leftJoinPlanets()
+      .where((builder) => {
+        if (planet) return builder.where({ planet_id: planet })
+        return true
+      })
+      .andWhere((builder) => {
+        if (birthDate) {
+          const dateObj = new Date(birthDate)
+          const start = dateObj.toISOString()
+          dateObj.setDate(dateObj.getDate() + 1)
+          const end = dateObj.toISOString()
+          return builder.whereBetween('born_at', [start, end])
+        }
+        return true
+      })
+      .limit(pageSize)
+      .offset(page === 1 ? 0 : page * pageSize)
+      .options({ nestTables: true })
+
+    const characters = charactersData.map(
+      createCharacterWithPlanetFromDbResponse
+    )
+    return {
+      pagination: { total: this.getTotalRecords(), page, pageSize },
+      nodes: characters,
+    }
+  }
+
+  getCharacterById(id) {
+    return this.leftJoinPlanets()
+      .where({ 'characters.id': id })
+      .first('*')
+      .then(createCharacterWithPlanetFromDbResponse)
+  }
+
+  leftJoinPlanets() {
+    return this.knex
+      .select(
+        this.knex.raw('to_json(characters.*) as character'),
+        this.knex.raw('to_json(planets.*) as planet')
+      )
+      .from('characters')
+      .leftJoin('planets', 'planets.id', 'characters.planet_id')
+  }
+
+  getTotalRecords() {
+    return this.knex
+      .count('*')
+      .from('characters')
+      .then((data) => data[0].count)
+      .then(Number)
+  }
+}
+
+export default CharactersService
