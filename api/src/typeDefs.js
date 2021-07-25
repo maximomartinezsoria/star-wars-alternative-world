d51d247746a309ed3cdbcec7f640c6140914aa73
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 01:53:47 -0300

    [Api] Create GraqhQL types

--- api/src/typeDefs.js
+++ api/src/typeDefs.js
@@ -3,8 +3,51 @@ import * as ApolloServer from 'apollo-server-koa'
 const { gql } = ApolloServer
 
 const typeDefs = gql`
+  type Planet {
+    id: ID!
+    name: String!
+    description: String!
+    code: String!
+    pictureUrl: String!
+    population: Int!
+    characters(limit: Int): [Character]
+  }
+
+  type Character {
+    id: ID!
+    name: String!
+    description: String!
+    bornAt: String!
+    pictureUrl: String!
+    planet: Planet!
+    friends(limit: Int): [Character]
+  }
+
+  type Pagination {
+    total: Int!
+    page: Int!
+    pageSize: Int!
+  }
+
+  type PlanetsResponse {
+    pagination: Pagination
+    nodes: [Planet!]
+  }
+
+  type CharactersResponse {
+    pagination: Pagination
+    nodes: [Character!]
+  }
+
   type Query {
-    hello: String
+    planets(page: Int, pageSize: Int): PlanetsResponse!
+    characters(
+      page: Int
+      pageSize: Int
+      planet: Int
+      birthDate: String
+    ): CharactersResponse!
+    character(id: ID!): Character
   }
 `
 
