35e847a6fbdb19f754b06e302f4e544913cbcf23
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 16:42:56 -0300

    [App] Space - empty state

--- app/src/containers/Space.jsx
+++ app/src/containers/Space.jsx
@@ -1,5 +1,31 @@
 import Layout from '../components/Layout'
+import PlanetLoader from '../components/icons/PlanetLoader'
+import styled from 'styled-components'
+
+const SpaceEmptyStateStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 8rem;
+
+  svg {
+    margin-bottom: 4rem;
+  }
+
+  h2 {
+    margin-bottom: 2.4rem;
+  }
+`
 
 export default function Space() {
-  return <Layout title="Space"></Layout>
+  return (
+    <Layout title="Space">
+      <SpaceEmptyStateStyles>
+        <PlanetLoader />
+        <h2>Space doesn't have to be so empty.</h2>
+        <button>Create Planet</button>
+      </SpaceEmptyStateStyles>
+    </Layout>
+  )
 }
