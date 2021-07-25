0c448a775a3c78e1354d9787fb3fba21fb815d3d
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 01:57:23 -0300

    [Api] define resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,6 +1,25 @@
 const resolvers = {
   Query: {
-    hello: () => 'Hello, World!',
+    planets(_, { page, pageSize }, { dataSources: { planetsService } }) {
+      return planetsService.getAllPlanets(page, pageSize)
+    },
+
+    characters(
+      _,
+      { page, pageSize, planet, birthDate },
+      { dataSources: { charactersService } }
+    ) {
+      return charactersService.getAllCharacters(
+        page,
+        pageSize,
+        planet,
+        birthDate
+      )
+    },
+
+    character(_, { id }, { dataSources: { charactersService } }) {
+      return charactersService.getCharacterById(id)
+    },
   },
 }
 
