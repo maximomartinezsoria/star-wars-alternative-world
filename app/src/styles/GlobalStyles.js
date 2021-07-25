96827fa96bb66195631db8710a718f485e0ad40a
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 15:26:36 -0300

    [App] Body background color

--- app/src/styles/GlobalStyles.js
+++ app/src/styles/GlobalStyles.js
@@ -1,10 +1,14 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-:root {
+  :root {
     --primary: #121C33;
     --white: #fff;
     --gray: #f5f5f5;
+  }
+
+  body {
+    background-color: var(--gray);
   }
 `
 
