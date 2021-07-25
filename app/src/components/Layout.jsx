87e5334ad8f25c4350a36c7b7bb923f8baf700a9
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 14:59:50 -0300

    [App] Change main to Fragment in Layout

--- app/src/components/Layout.jsx
+++ app/src/components/Layout.jsx
@@ -3,13 +3,13 @@ import Navigation from './Navigation'
 
 export default function Layout({ title, children }) {
   return (
-    <main>
+    <>
       <header>
         <h1>{title}</h1>
       </header>
       <Navigation />
       {children}
-    </main>
+    </>
   )
 }
 
