6065b871788b2a32d4db08dbea4d7cf435d398aa
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 13:39:27 -0300

    [App] Add routing

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,16 @@
-function App() {
-	return <main>Hello, World!</main>
-}
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Characters from './containers/Characters'
+import Space from './containers/Space'
 
-export default App
+export default function App() {
+  return (
+    <main>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Space} />
+          <Route path="/characters" component={Characters} />
+        </Switch>
+      </Router>
+    </main>
+  )
+}
