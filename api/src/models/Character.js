56949423df4c3f084dc611603b51ca79d12b1e5c
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 01:54:56 -0300

    [Api] Create Character model

---
+++ api/src/models/Character.js
@@ -0,0 +1,12 @@
+class Character {
+  constructor(id, name, description, bornAt, pictureUrl, planet) {
+    this.id = id
+    this.name = name
+    this.description = description
+    this.bornAt = bornAt
+    this.pictureUrl = pictureUrl
+    this.planet = planet
+  }
+}
+
+export default Character
