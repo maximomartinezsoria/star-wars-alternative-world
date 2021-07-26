44bdb52dd94526c4cd94bd093dcf9034922dde5d
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 21:46:54 -0300

    [App] Add Card component

---
+++ app/src/components/Card.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const CardStyles = styled.article`
+  border-radius: 16px;
+  transition: background-color 0.1s ease-in;
+  background-color: var(--white);
+
+  figure {
+    margin: 0;
+  }
+
+  &:hover {
+    background-color: var(--gray);
+  }
+`
+
+const TextContainerStyles = styled.div`
+  padding: 0.8rem 1.6rem 1.6rem;
+
+  p {
+    font-size: 1.2rem;
+    opacity: 0.5;
+  }
+`
+
+export default function Card({ title, image, text }) {
+  return (
+    <CardStyles>
+      <figure>
+        <img src={image} alt={title} />
+      </figure>
+      <TextContainerStyles>
+        <h3>{title}</h3>
+        <p>{text}</p>
+      </TextContainerStyles>
+    </CardStyles>
+  )
+}
+
+Card.propTypes = {
+  title: PropTypes.string,
+  image: PropTypes.string,
+  text: PropTypes.string,
+}
