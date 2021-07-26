127ae397f65ac00243d3d607ddfea77175322455
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-25 22:09:10 -0300

    [App] Add Grid styles

---
+++ app/src/styles/Grid.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Grid = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-gap: 1.5rem;
+`
+
+export default Grid
