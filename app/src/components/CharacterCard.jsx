abebdff7ca8334aee3d6040f16dd52f9efc1fa18
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-26 10:18:59 -0300

    [App] Add CharacterCard

---
+++ app/src/components/CharacterCard.jsx
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const CharacterCardStyles = styled.article`
+  display: flex;
+
+  figure {
+    width: 25%;
+    padding-bottom: 25%;
+    margin-right: 1.5rem;
+  }
+
+  div {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+`
+
+export default function CharacterCard({ character }) {
+  return (
+    <CharacterCardStyles>
+      <figure class="image-container">
+        <img src={character.pictureUrl} alt={character.name} />
+      </figure>
+      <div>
+        <h4>{character.name}</h4>
+        <p>{character.text}</p>
+      </div>
+    </CharacterCardStyles>
+  )
+}
+
+CharacterCard.propTypes = {
+  character: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    pictureUrl: PropTypes.string.isRequired,
+  }),
+}
