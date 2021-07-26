0f12bf3f0a9eff7a59fc0dba995ea08cc2585c16
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-26 10:52:19 -0300

    [App] Use Sidebar in Characters

--- app/src/containers/Characters.jsx
+++ app/src/containers/Characters.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import Layout from '../components/Layout'
 import GET_CHARACTERS from '../queries/getCharacters'
 import Grid from '../styles/Grid'
 import Card from '../components/Card'
+import Sidebar from '../components/Sidebar'
 import EmptyState from '../components/EmptyState'
 
 export default function Characters() {
+  const [selectedCharacter, setSelectedCharacter] = useState(null)
   const { loading, error, data } = useQuery(GET_CHARACTERS, {
     variables: { pageSize: 12 },
   })
@@ -23,11 +26,33 @@ export default function Characters() {
               title={character.name}
               image={character.pictureUrl}
               text="10 friends"
+              onClick={() => {
+                setSelectedCharacter(character)
+              }}
             />
           ))}
         </Grid>
       ) : (
         <EmptyState entity="Character" />
+      )}
+
+      {selectedCharacter && (
+        <Sidebar
+          title={selectedCharacter.name}
+          text={selectedCharacter.description}
+          descriptionList={[
+            { title: 'Planet', text: selectedCharacter.planet.name },
+            { title: 'Friends', text: `${data.characters.nodes.length}` },
+          ]}
+          charactersListTitle="Friends"
+          characters={data.characters.nodes.map((character) => ({
+            id: character.id,
+            name: character.name,
+            text: character.description,
+            pictureUrl: character.pictureUrl,
+          }))}
+          onClose={() => setSelectedCharacter(null)}
+        />
       )}
     </Layout>
   )
