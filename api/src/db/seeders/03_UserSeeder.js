c60a698bc43b29da671e4b344fbb249561068d76
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-27 12:21:47 -0300

    [Api] Encrypt password in UserSeeder

--- api/src/db/seeders/03_UserSeeder.js
+++ api/src/db/seeders/03_UserSeeder.js
@@ -1,8 +1,9 @@
+import bcrypt from 'bcrypt'
+
 export async function seed(knex) {
   const tableName = 'users'
+  const password = await bcrypt.hash('admin', 10)
   return knex(tableName)
     .del()
-    .then(() =>
-      knex(tableName).insert([{ username: 'admin', password: 'admin' }])
-    )
+    .then(() => knex(tableName).insert([{ username: 'admin', password }]))
 }
