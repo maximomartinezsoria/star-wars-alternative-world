ae5c2b24f3799788f9e2125983b264337a04fb42
Maximo Martinez Soria <maximo.martinezsoria@gmail.com>
2021-07-28 10:43:47 -0300

    [App] add getCharacterDetail query

---
+++ app/src/queries/getCharacterDetail.js
@@ -0,0 +1,23 @@
+import { gql } from '@apollo/client'
+
+const GET_CHARACTER_DETAILS = gql`
+  query getCharacterDetail($characterId: ID!) {
+    character(id: $characterId) {
+      id
+      name
+      description
+      planet {
+        id
+        name
+      }
+      friends {
+        id
+        name
+        description
+        pictureUrl
+      }
+    }
+  }
+`
+
+export default GET_CHARACTER_DETAILS
